27f952b81b188442a54c0c54ba9c399e57133d4e
ChiSenSan <rooter1400@gmail.com>
2017-02-04 21:19:31 +0900

    a

--- GameTest/test01/OnClickListener.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "View.h"
-
-class OnClickListener {
-public:
-	virtual ~OnClickListener();
-	virtual void OnClick(View* view);
-};
--- GameTest/test01/View.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class View {
-public:
-	View();
-	virtual ~View();
-	virtual void Update();
-	virtual void Draw();
-};
